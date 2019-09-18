8fd10b55f5a8e6499aafa11a90e78145618a817d
lijianliang <lijianliang@wecash-mail.com>
2019-09-18 14:55:26 +0800

    提币

--- novadax/request_client.py
+++ novadax/request_client.py
@@ -1,16 +1,15 @@
 from novadax.impl import HTTPClient
 
-class RequestClient(object):
-    def __init__(self, access_key = None, secret_key = None, url = 'https://api.novadax.com'):
-        self._http = HTTPClient(url, access_key, secret_key)
 
+class RequestClient(object):
+    def __init__(self, access_key=None, secret_key=None, url='https://api.novadax.com'):
+        self._http = HTTPClient(url, access_key, secret_key)
 
     def get_timestamp(self):
         return self._http.get('/v1/common/timestamp')
 
     def list_symbols(self):
         return self._http.get('/v1/common/symbols')
-
 
     def list_tickers(self):
         return self._http.get('/v1/market/tickers')
@@ -20,25 +19,25 @@ class RequestClient(object):
             'symbol': symbol
         })
 
-    def get_depth(self, symbol, limit = 100):
+    def get_depth(self, symbol, limit=100):
         return self._http.get('/v1/market/depth', {
             'symbol': symbol,
             'limit': limit
         })
 
-    def list_trades(self, symbol, limit = 100):
+    def list_trades(self, symbol, limit=100):
         return self._http.get('/v1/market/trades', {
             'symbol': symbol,
             'limit': limit
         })
-
 
     def get_order(self, _id):
         return self._http.get_with_auth('/v1/orders/get', {
             'id': _id
         })
 
-    def list_orders(self, symbol, status = None, from_id = None, to_id = None, from_timestamp = None, to_timestamp = None, limit = 100):
+    def list_orders(self, symbol, status=None, from_id=None, to_id=None, from_timestamp=None, to_timestamp=None,
+                    limit=100):
         return self._http.get_with_auth('/v1/orders/list', {
             'symbol': symbol,
             'status': status,
@@ -49,7 +48,7 @@ class RequestClient(object):
             'limit': limit
         })
 
-    def create_order(self, symbol, _type, side, price = None, amount = None, value = None):
+    def create_order(self, symbol, _type, side, price=None, amount=None, value=None):
         return self._http.post_with_auth('/v1/orders/create', {}, {
             'symbol': symbol,
             'type': _type,
@@ -69,6 +68,13 @@ class RequestClient(object):
             'id': _id
         })
 
-
     def get_account_balance(self):
         return self._http.get_with_auth('/v1/account/getBalance')
+
+    def withdraw_coin(self, code, amount, toAddr, tag=None):
+        return self._http.post_with_auth('/v1/account/withdraw/coin', {}, {
+            'amount': amount,
+            'code': code,
+            'wallet': toAddr,
+            'tag': tag
+        })
