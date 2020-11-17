866ab8884dcdd55c814bbfc5a7736e75b81b2994
qiujun <qiujun@wecash.net>
2020-11-17 16:24:27 +0800

    v1.1.0

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ print(setuptools.find_packages())
 
 setuptools.setup(
     name="novadax",
-    version="1.0.3",
+    version="1.1.0",
     author="NovaDAX",
     author_email="support@novadax.com",
     description="NovaDAX API SDK",
     url="https://doc.novadax.com/",
-    packages=["novadax", "novadax.exception", "novadax.impl"],
+    packages=["novadax", "novadax.exception", "novadax.kernel"],
     install_requires=['requests']
 )
