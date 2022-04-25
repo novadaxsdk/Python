fffe0c7dd86b54211d178b05ccec9a1d61b4a77b
wecash <lijianliang@wecash.net>
2022-04-25 11:59:03 +0800

    check api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="novadax",
-    version="1.1.3",
+    version="1.1.4",
     author="NovaDAX",
     author_email="support@novadax.com",
     description="NovaDAX API SDK",
